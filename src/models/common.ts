e20b0a801a912b3303639f147e8634a8a7c9e5a8
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-06-13 00:45:48 +0700

    13 - Add student apis

--- src/models/common.ts
+++ src/models/common.ts
@@ -8,3 +8,12 @@ export interface ListResponse<T> {
   data: T[];
   pagination: PaginationParams;
 }
+
+export interface ListParams {
+  _page: number;
+  _limit: number;
+  _sort: string;
+  _order: 'asc' | 'desc';
+
+  [key: string]: any;
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './city';
 export * from './common';
+export * from './student';
