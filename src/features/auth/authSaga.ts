fb71e08c10ec4c93b553e4b354dcc814c4ea9bce
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-06-14 13:02:01 +0700

    Implement auth saga logic

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,22 +1,41 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { fork, take, takeLatest } from 'redux-saga/effects';
+import { call, delay, fork, put, take } from 'redux-saga/effects';
 import { authActions, LoginPayload } from './authSlice';
 
 function* handleLogin(payload: LoginPayload) {
-  console.log('Handle login', payload);
+  try {
+    yield delay(1000);
+
+    localStorage.setItem('access_token', 'fake_token');
+    yield put(
+      authActions.loginSuccess({
+        id: 1,
+        name: 'Easy Frontend',
+      })
+    );
+  } catch (error) {
+    yield put(authActions.loginFailed(error.message));
+  }
+  // redirect to admin page
 }
 
 function* handleLogout() {
-  console.log('Handle logout');
+  yield delay(500);
+  localStorage.removeItem('access_token');
+  // redirect to login page
 }
 
 function* watchLoginFlow() {
   while (true) {
-    const action: PayloadAction<LoginPayload> = yield take(authActions.login.type);
-    yield fork(handleLogin, action.payload);
+    const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+
+    if (!isLoggedIn) {
+      const action: PayloadAction<LoginPayload> = yield take(authActions.login.type);
+      yield fork(handleLogin, action.payload);
+    }
 
     yield take(authActions.logout.type);
-    yield fork(handleLogout);
+    yield call(handleLogout);
   }
 }
 
