9db04806ac48c0045559d0a1a06062513544d040
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-06-17 14:16:54 +0700

    26 - Implement StatisticItem

---
+++ src/features/dashboard/components/StatisticItem.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { Box, Paper, makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    padding: theme.spacing(2),
+    border: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export interface StatisticItemProps {
+  icon: React.ReactElement;
+  label: string;
+  value: string | number;
+}
+
+export default function StatisticItem({ icon, label, value }: StatisticItemProps) {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Box>{icon}</Box>
+
+      <Box>
+        <Typography variant="h5" align="right">
+          {value}
+        </Typography>
+        <Typography variant="caption">{label}</Typography>
+      </Box>
+    </Paper>
+  );
+}
--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { Box, Grid, LinearProgress, makeStyles } from '@material-ui/core';
+import { ChatBubble, ChatRounded, LinearScaleSharp, PeopleAlt } from '@material-ui/icons';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import React, { useEffect } from 'react';
+import StatisticItem from './components/StatisticItem';
 import {
   dashboardActions,
   selectDashboardLoading,
@@ -9,6 +12,19 @@ import {
   selectRankingByCityList,
 } from './dashboardSlice';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+    paddingTop: theme.spacing(1),
+  },
+
+  loading: {
+    position: 'absolute',
+    top: theme.spacing(-1),
+    width: '100%',
+  },
+}));
+
 export default function Dashboard() {
   const dispatch = useAppDispatch();
   const loading = useAppSelector(selectDashboardLoading);
@@ -16,6 +32,8 @@ export default function Dashboard() {
   const highestStudentList = useAppSelector(selectHighestStudentList);
   const lowestStudentList = useAppSelector(selectLowestStudentList);
   const rankingByCityList = useAppSelector(selectRankingByCityList);
+
+  const classes = useStyles();
 
   console.log({
     loading,
@@ -29,5 +47,45 @@ export default function Dashboard() {
     dispatch(dashboardActions.fetchData());
   }, [dispatch]);
 
-  return <div>Dashboard</div>;
+  return (
+    <Box className={classes.root}>
+      {/* Loading */}
+      {loading && <LinearProgress className={classes.loading} />}
+
+      {/* Statistic Section */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize="large" color="primary" />}
+            label="male"
+            value={statistics.maleCount}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<ChatRounded fontSize="large" color="primary" />}
+            label="female"
+            value={statistics.femaleCount}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<ChatBubble fontSize="large" color="primary" />}
+            label="mark >= 8"
+            value={statistics.highMarkCount}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<LinearScaleSharp fontSize="large" color="primary" />}
+            label="mark <= 5"
+            value={statistics.lowMarkCount}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
