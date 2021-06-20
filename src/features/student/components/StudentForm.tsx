3cd298747ba1d91f360327a819262dd12e81ea96
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-06-20 22:17:36 +0700

    44 - Validate student form with yup

--- src/features/student/components/StudentForm.tsx
+++ src/features/student/components/StudentForm.tsx
@@ -5,17 +5,51 @@ import { selectCityOptions } from 'features/city/citySlice';
 import { Student } from 'models';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 export interface StudentFormProps {
   initialValues?: Student;
   onSubmit?: (formValues: Student) => void;
 }
 
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Please enter name.')
+    .test('two-words', 'Please enter at least two words', (value) => {
+      if (!value) return true;
+
+      const parts = value?.split(' ') || [];
+      return parts.filter((x) => Boolean(x)).length >= 2;
+    }),
+  age: yup
+    .number()
+    .positive('Please enter a positive number.')
+    .min(18, 'Min is 18')
+    .max(60, 'Max is 60')
+    .integer('Please enter an integer.')
+    .required('Please enter age.')
+    .typeError('Please enter a valid number.'),
+  mark: yup
+    .number()
+    .min(0, 'Min is 0')
+    .max(10, 'Max is 10')
+    .required('Please enter mark.')
+    .typeError('Please enter a valid number.'),
+  gender: yup
+    .string()
+    .oneOf(['male', 'female'], 'Please select either male or female.')
+    .required('Please select gender.'),
+  city: yup.string().required('Please select city.'),
+});
+
 export default function StudentForm({ initialValues, onSubmit }: StudentFormProps) {
   const cityOptions = useAppSelector(selectCityOptions);
 
   const { control, handleSubmit } = useForm<Student>({
     defaultValues: initialValues,
+    resolver: yupResolver(schema),
   });
 
   const handleFormSubmit = (formValues: Student) => {
