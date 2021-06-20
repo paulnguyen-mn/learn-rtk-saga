6c57ef8719dcc800dede08429aac3e36fb160e56
Hau Nguyen <paulnguyen.mn@gmail.com>
2021-06-20 21:42:36 +0700

    42 - Implement RadioGroupField

---
+++ src/components/FormFields/RadioGroupField.tsx
@@ -0,0 +1,50 @@
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import { FormHelperText } from '@material-ui/core';
+import * as React from 'react';
+import { Control, useController } from 'react-hook-form';
+
+export interface RadioOption {
+  label?: string;
+  value: number | string;
+}
+
+export interface RadioGroupFieldProps {
+  name: string;
+  control: Control<any>;
+  label?: string;
+  disabled?: boolean;
+  options: RadioOption[];
+}
+
+export function RadioGroupField({ name, control, label, disabled, options }: RadioGroupFieldProps) {
+  const {
+    field: { value, onChange, onBlur },
+    fieldState: { invalid, error },
+  } = useController({
+    name,
+    control,
+  });
+
+  return (
+    <FormControl disabled={disabled} margin="normal" component="fieldset" error={invalid}>
+      <FormLabel component="legend">{label}</FormLabel>
+
+      <RadioGroup name={name} value={value} onChange={onChange} onBlur={onBlur}>
+        {options.map((option) => (
+          <FormControlLabel
+            key={option.value}
+            value={option.value}
+            control={<Radio />}
+            label={option.label}
+          />
+        ))}
+      </RadioGroup>
+
+      <FormHelperText>{error?.message}</FormHelperText>
+    </FormControl>
+  );
+}
